6a2a06c052fa9fb42e5b97bca83c117582b98c5f
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2024-04-22 18:29:45 -0400

    Monad en Pascal Triangle

--- Monad/monads.hs
+++ Monad/monads.hs
@@ -1,1 +1,27 @@
--- Que es un monada 
+-- What is a monad  
+-- Una mónada en Haskell es una clase de tipos que proporciona una forma de secuenciar cálculos. 
+-- Le permite encadenar múltiples cálculos mientras maneja efectos como el estado
+
+-- para un monad se debe definir el operador de secuencia el >>= y el return
+
+import Control.Monad.State
+import Control.Monad (replicateM)
+
+nextRow :: [Integer] -> [Integer]
+nextRow row = zipWith (+) (0:row) (row ++ [0])
+
+
+pascalTriangle :: Int -> [[Integer]]
+pascalTriangle n = evalState (replicateM n generateRow) [1]
+    where
+        generateRow :: State [Integer] [Integer]
+        generateRow = do
+            row <- get
+            put (nextRow row)
+            return row
+
+main :: IO ()
+main = do
+    let n = 5
+    let triangle = pascalTriangle n
+    mapM_ print triangle
